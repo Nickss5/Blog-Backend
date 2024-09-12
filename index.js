1785fd52e69bb67efbea4637c5db3a858de1662d
Nickss5 <medagoninishma@gmail.com>
2024-09-12 20:15:05 +0530

    Update CORS origin for Vercel frontend

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const connectDB = async () => {
 app.use(express.json());
 app.use(cookieParser());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://blog-publishing-platform-fullstack1.vercel.app/", credentials: true }));
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
